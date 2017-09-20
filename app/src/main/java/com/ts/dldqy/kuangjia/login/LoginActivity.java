c57dc33fe62ef4a10d2ac36e0d768d70eb1a0251
KingCold <1120618826@qq.com>
2017-09-20 17:06:06 +0800

    初始化控件

--- app/src/main/java/com/ts/dldqy/kuangjia/login/LoginActivity.java
+++ app/src/main/java/com/ts/dldqy/kuangjia/login/LoginActivity.java
@@ -1,24 +1,55 @@
 package com.ts.dldqy.kuangjia.login;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.ts.dldqy.kuangjia.R;
 import com.ts.dldqy.kuangjia.base.BaseActivity;
-import com.ts.dldqy.kuangjia.utils.permissionutils.PermissionEnum;
+import com.ts.dldqy.kuangjia.view.LineEditText;
 
-import java.util.ArrayList;
 
+/**
+ * 登录界面
+ * @author wh
+ * 
+ */
 public class LoginActivity extends BaseActivity {
 
-    /**
-     *
-     * @param savedInstanceState
-     */
-
+    private Context context;
+    private LineEditText loginEditTextUser,loginEditTextPwd;
+    private Button loginButton;
+    private String sLoginUser,sLoginPwd;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initPost();
+    }
+
+    /**
+     * 发送请求
+     */
+    private void initPost() {
+        if(!getConnectivityStatus()){
+            myToast(context,context.getResources().getString(R.string.toast_no_intent));
+        }else{
+            
+        }
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        context=this;
+        loginEditTextUser=$(R.id.loginEditTextUser);
+        loginEditTextPwd=$(R.id.loginEditTextPwd);
+        loginButton=$(R.id.loginButton);
+        sLoginUser=loginEditTextUser.getText().toString();
+        sLoginPwd=loginEditTextPwd.getText().toString();
     }
 
     @Override
