73bc61d7477cd4892c87503b9225ed4854c05135
HSLSH <596945463@qq.com>
2017-09-21 09:42:04 +0800

    税源大户

--- app/src/main/java/com/ts/dldqy/kuangjia/largetaxsource/activity/LargeTaxSourceActivity.java
+++ app/src/main/java/com/ts/dldqy/kuangjia/largetaxsource/activity/LargeTaxSourceActivity.java
@@ -1,8 +1,18 @@
 package com.ts.dldqy.kuangjia.largetaxsource.activity;
 
+import android.os.Bundle;
+
+import com.ts.dldqy.kuangjia.base.BaseActivity;
+
 /**
+ * 税源大户页面
  * Created by Administrator on 2017/9/21 0021.
  */
 
-public class LargeTaxSourceActivity {
+public class LargeTaxSourceActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
