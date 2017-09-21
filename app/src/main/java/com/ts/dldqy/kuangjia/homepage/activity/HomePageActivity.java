87cb70929814fc476512b24c54b6331b93cfb77f
KingCold <1120618826@qq.com>
2017-09-21 13:35:30 +0800

    首页

---
+++ app/src/main/java/com/ts/dldqy/kuangjia/homepage/activity/HomePageActivity.java
@@ -0,0 +1,15 @@
+package com.ts.dldqy.kuangjia.homepage.activity;
+
+import android.os.Bundle;
+
+import com.ts.dldqy.kuangjia.R;
+import com.ts.dldqy.kuangjia.base.BaseActivity;
+
+public class HomePageActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
